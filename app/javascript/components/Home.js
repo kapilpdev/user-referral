1dab34d1f880ff46b9eca88788f018cf0bd7cc1c
Kapil Patel <kapilppatel03@gmail.com>
2023-05-26 23:31:06 +0530

    Added home controller on rails and added components for react

---
+++ app/javascript/components/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Referral Invites</h1>
+        <p className="lead">
+          A list of referral invitations.
+        </p>
+        <hr className="my-4" />
+        <Link
+          to="/referrals"
+          className="btn btn-lg custom-button"
+          role="button"
+        >
+          View referrals
+        </Link>
+      </div>
+    </div>
+  </div>
+);
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users, defaults: { format: :json },
                      controllers: { sessions: 'users/sessions',
                                     registrations: 'users/registrations',
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
                                   }
 
   get '/health_check', to: proc { [200, {}, ['success']] }
+  root 'home#index'
 end
