8a11828f854d6fa3d7e86e46b5ab42d71873e25e
Kapil Patel <kapilppatel03@gmail.com>
2023-05-28 03:53:01 +0530

    update referral status when sign in via link

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,10 +4,12 @@ module Users
   # app/controllers/users/registrations_controller.rb
   class RegistrationsController < Devise::RegistrationsController
     before_action :authorize_request, except: :create
+    before_action :set_referrer
     protect_from_forgery with: :null_session
 
     def create
       user = User.new(sign_up_params)
+      user.referrer = @referrer
       token = JsonWebToken.encode(user_id: user.id)
       if user.save
         render json: { id: user.id, email: user.email,
@@ -18,6 +20,10 @@ module Users
     end
 
     private
+
+    def set_referrer
+      @referrer = User.find_by(referral_token: params[:referral_token]) if params[:referral_token]
+    end
 
     def sign_up_params
       params.require(:user).permit(:email, :password, :password_confirmation)
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,15 @@ class User < ApplicationRecord
   belongs_to :referrer, class_name: 'User', foreign_key: 'referrer_id', optional: true
 
   before_create :generate_referral_token
+  after_create :update_referral_status
 
   private
 
   def generate_referral_token
     self.referral_token = SecureRandom.hex(5)
+  end
+
+  def update_referral_status
+    ReferralInvitation.where(email: email).update(status: 'accepted')
   end
 end
