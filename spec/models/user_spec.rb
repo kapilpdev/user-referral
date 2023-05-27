f07a2a30a678062f2165029bb648579635c185c3
Kapil Patel <kapilppatel03@gmail.com>
2023-05-28 02:51:29 +0530

    Fixed rubocop issues

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'associations' do
-    it { should belong_to(:referrer).class_name('User').optional(:true) }
+    it { should belong_to(:referrer).class_name('User').optional(true) }
     it { should have_many(:referral_invitations) }
   end
 end
